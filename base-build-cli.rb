95b73245508404ea2df1cb0caad19a777bd25f9c
Andrew S Thompson <admin@entercloud.info>
2015-09-30 00:38:08 -0700

    base-build tool with install all and debug cli options added

--- base-build-cli.rb
+++ base-build-cli.rb
@@ -4,8 +4,12 @@ options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: base-deploy.rb [options]"
 
-  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+  opts.on("-v", "--[no-]verbose", "Detailed output -debug") do |v|
     options[:verbose] = v
+end
+
+  opts.on("-a", "--all", "install all dependencies and required settings based on state of system and state of file definition") do |a|
+    options[:all] = a 
   end
 end.parse!
 
